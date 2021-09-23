f67fd5271e45550a4f7b111cdca21691e19e3cd6
ammarr96 <amarluba_96@hotmail.com>
2021-09-23 10:12:03 +0200

    tests added

---
+++ app/src/androidTest/java/com/amar/comicstestapp/repository/ComicsRepositoryTest.kt
@@ -0,0 +1,16 @@
+package com.amar.comicstestapp.repository
+
+import org.junit.Assert.*
+
+import org.junit.Test
+
+class ComicsRepositoryTest {
+
+    @Test
+    fun setComicList() {
+    }
+
+    @Test
+    fun getComic() {
+    }
+}
